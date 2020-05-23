d748e45bb56127522c7631b2850631be4333db8b
Mahfuz Raihan <mahfuz_raihan@outlook.com>
2020-05-23 17:01:13 +0600

    Month

---
+++ uri1052.py
@@ -0,0 +1,26 @@
+month_number = int(input())
+
+if month_number == 1:
+	print("January")
+elif month_number == 2:
+	print("February")
+elif month_number == 3:
+	print("March")
+elif month_number == 4:
+	print("April")
+elif month_number == 5:
+	print("May")
+elif month_number == 6:
+	print("June")
+elif month_number == 7:
+	print("July")
+elif month_number == 8:
+	print("August")
+elif month_number == 9:
+	print("September")
+elif month_number == 10:
+	print("October")
+elif month_number == 11:
+	print("November")
+elif month_number == 12:
+	print("December")

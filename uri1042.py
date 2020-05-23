b5178a9bf76ae08ae6ff931c77d91c0a6f2a93c6
Mahfuz Raihan <mahfuz_raihan@outlook.com>
2020-05-23 16:50:13 +0600

    Simple Sort

--- uri1042.py
+++ uri1042.py
@@ -1,47 +1,17 @@
-x,y,z = input().split()
+numbers = list(map(int,input().strip().split()))[:3]
 
-j = x
-k = y
-l = z
+a = numbers[0]
+b = numbers[1]
+c = numbers[2]
 
-a = int(x)
-b = int(y)
-c = int(z)
+order = [a,b,c]
+order.sort()
 
-if a > b and a > c:
-    big = a
-if a < b and b > c:
-    big = b
-if c > a and c > b:
-    big = c
-
-if a < b and a < c:
-    small = a
-if b < a and b < c:
-    small = b
-if c < a and c < b:
-    small = c
-
-if big == a and small == b:
-    middle = c
-if big == b and small == a:
-    middle = c
-if big == b and small == c:
-    middle = a
-if big == c and small == b:
-    middle = a
-
-if big == a and samll == c:
-    middle == b
-if big == c and small == a:
-    middle = b
-    
-print(small)
-print(middle)
-print(big)
-
+print(order[0])
+print(order[1])
+print(order[2])
 print()
 
-print(j)
-print(k)
-print(l)
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])

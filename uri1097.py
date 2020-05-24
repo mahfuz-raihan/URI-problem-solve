d57a7f649935a65d52a8736e833c7f4fb538de80
Mahfuz Raihan <mahfuz_raihan@outlook.com>
2020-05-24 18:30:17 +0600

    Sequence IJ 3

---
+++ uri1097.py
@@ -0,0 +1,11 @@
+J = 7
+i = -1
+for i in range(1, 6):
+	i = I+2
+	J = J
+	print("I={0} J={1}".format(i, J))
+	print("I={0} J={1}".format(i, J-1))
+	print("I={0} J={1}".format(i, J-2))
+	J +=2
+	I = i
+	

9439428510ab8b889d5a602603da5fbfd572f1ad
Mahfuz Raihan <mahfuz_raihan@outlook.com>
2020-06-01 00:45:52 +0600

    Sum of Consecutive Odd Numbers I

--- uri1071.py
+++ uri1071.py
@@ -1,10 +1,10 @@
-# ***unsolved***
 
-x = int(input())
-y = int(input())
+X = int(input())
+Y = int(input())
+
 count = 0
-for i in (y, x, 1):
-	if i % 2 != 0:
-		count += i
+for i in range((Y+1), X):
+	if (i % 2 != 0):
+		count +=i
 
 print(count)
